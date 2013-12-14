af9346279d7c9d21d8b4600a21d94e4e5cdd8fda
Flafla2 <awesomesauceface@aol.com>
2013-12-13 23:24:17 -0500

    Base stuff

---
+++ One/src/com/remote/ld/one/One.java
@@ -0,0 +1,18 @@
+package com.remote.ld.one;
+
+import com.remote.remote2d.engine.Remote2D;
+import com.remote.remote2d.engine.Remote2DGame;
+
+public class One extends Remote2DGame {
+	
+	public static void main(String[] args)
+	{
+		Remote2D.startRemote2D(new One());
+	}
+
+	@Override
+	public void initGame() {
+		
+	}
+	
+}
